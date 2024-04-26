5c403e6addc001d85af1b4600ef2fe4a422291a6
Anthony Margerand <anthony@happywait.com>
2024-04-26 15:05:09 +0200

    Fix

--- src/core/object.cpp
+++ src/core/object.cpp
@@ -897,17 +897,6 @@ void init_object(py::module_ &m)
             return poh.getObjectHandle();
         });
     py::class_<QPDFDocumentHelper, std::shared_ptr<QPDFDocumentHelper>>(m, "DocumentHelper");
-//        .def(
-//            "__eq__",
-//            [](QPDFObjectHelper &self, QPDFObjectHelper &other) {
-//                // Object helpers are equal if their object handles are equal
-//                return objecthandle_equal(
-//                    self.getObjectHandle(), other.getObjectHandle());
-//            },
-//            py::is_operator())
-//        .def_property_readonly("obj", [](QPDFObjectHelper &poh) -> QPDFObjectHandle {
-//            return poh.getObjectHandle();
-//        });
 
     m.def("_encode", [](py::handle handle) { return objecthandle_encode(handle); });
     m.def("unparse", [](py::object obj) -> py::bytes {
